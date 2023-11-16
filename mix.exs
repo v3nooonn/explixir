8238afcf919abcfde6d7426c168c2348952ddf0a
v3nooonn <v3nooom@outlook.com>
2023-11-16 15:41:36 +0800

    quit

--- mix.exs
+++ mix.exs
@@ -6,7 +6,8 @@ defmodule Explixir.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -16,6 +17,24 @@ defmodule Explixir.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
+    [
+      {:absinthe, "~> 1.7"},
+      {:absinthe_plug, "~> 1.5"},
+      {:phoenix, "~> 1.7"},
+      {:ecto, "~> 3.10.0"},
+      {:postgrex, "~> 0.17.3"},
+      {:gettext, "~> 0.23.1"},
+      {:jason, "~> 1.4"}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
     []
   end
 end
